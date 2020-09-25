5976762fee1cd4c3076775deb8ff114b62bb2844
João Lopes <linux-man@hotmail.com>
2020-09-25 13:38:11 +0100

    Demo prints min/max and histogram for each generator by @jminor

--- demo.lua
+++ demo.lua
@@ -7,6 +7,36 @@ c2 = mwc(0, 'nr') -- Multiply-with-carry (Numerical recipes params)
 m = twister(0) -- Mersenne twister
 
 for n = 1, 10 do
-   io.write(string.format("%8u%8u%8u%8u%16u\n", l1:random(0), l2:random(0), c1:random(0), c2:random(0), m:random(0)))
+    io.write(string.format("%8u%8u%8u%8u%16u\n", l1:random(0), l2:random(0), c1:random(0), c2:random(0), m:random(0)))
 end
 
+-- lets run them all a bunch of times and gather stats
+count = 10000
+names = {"l1", "l2", "c1", "c2", "m"}
+generators = {l1, l2, c1, c2, m}
+mins = {}
+maxs = {}
+bins = {}
+for i, gen in ipairs(generators) do
+    bins[i] = {}
+    for n = 1, count do
+        x = gen:random()
+        bin = math.floor(x*10) + 1
+        bins[i][bin] = (bins[i][bin] or 0) + 100/count
+        if mins[i] == nil or x < mins[i] then
+            mins[i] = x
+        end
+        if maxs[i] == nil or x > maxs[i] then
+            maxs[i] = x
+        end
+    end
+end
+
+print("name: min - max [histogram of each 10%]")
+for i = 1, #generators do
+    for j = 1, #bins[i] do
+        bins[i][j] = string.format("%2.0f%%", bins[i][j])
+    end
+    distribution = table.concat(bins[i], " ")
+    print(string.format("%2s: %f - %f [%s]", names[i], mins[i], maxs[i], distribution))
+end
